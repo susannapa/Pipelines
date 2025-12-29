193dfe6af4b33c0ac6ae779883ddc46ab4dd9caf
susannapa <64579633+susannapa@users.noreply.github.com>
2025-12-29 16:48:54 +0000

    Create 07_call_vcf_single.sh

---
+++ wgs-preprocessing/07_call_vcf_single.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -l tmem=3.6G          #physical memory limit
+#$ -l h_vmem=3.6G        #virtual memory limit
+#$ -l h_rt=120:00:00     #Wall time
+#$ -j y                  #merges STDOUT and STDERR
+#$ -N bamtovcf_single    #job name
+#$ -pe smp 4
+#$ -t 1-4                #Number of files: so you need the total number of bams you will run over 
+#$ -tc 4                 #maximal number of jobs you can submit
+
+#this script works for single samples
+
+# Load JAVA
+export JAVA_HOME=/share/apps/jdk1.8.0_131
+export PATH=/share/apps/jdk1.8.0_131/bin:${PATH}
+# Load Picard tools
+export PATH=/share/apps/genomics/picard-2.20.3/bin:${PATH}
+java -jar /share/apps/genomics/picard-2.20.3/bin/picard.jar -h
+# Load SAM tools
+export PATH=/share/apps/genomics/samtools-1.9/bin:${PATH}
+# Load GATK
+export PATH=/share/apps/genomics/gatk-4.1.2.0:${PATH}
+# Load python
+source /share/apps/source_files/python/python-3.6.9.source
+
+#cd where the recal bams are located
+cd /your/input/directory/recal
+
+inputfile=( *recal.bam  )
+k1="${inputfile[$SGE_TASK_ID-1]}"
+
+genome=/directory/to/genome/GRCh38.d1.vd1.fa
+
+gatk --java-options "-Xmx4g" HaplotypeCaller \
+     -R $genome \
+     -I $k1 \
+     -O /directory/vcf/${k1%%_.recal.bam}".vcf.gz" \
+               
